db148fccf0fc8008d44b4bb23b84c72c937c1f1b
023Marlon <w217020567@student.hccs.edu>
2025-05-08 08:34:49 +0500

    Update AppointmentBSTTest.java

--- src/test/java/AppointmentBSTTest.java
+++ src/test/java/AppointmentBSTTest.java
@@ -1,4 +1,3 @@
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,5 +8,30 @@ public class AppointmentBSTTest {
         PatientAppointment p1 = new PatientAppointment(1, "Test Patient", "09:00");
         bst.addAppointment(p1);
         assertNotNull(bst.getRoot());
+        assertEquals("09:00", bst.getRoot().data.appointmentTime);
+    }
+
+    @Test
+    public void testDuplicateAppointmentTime() {
+        AppointmentBST bst = new AppointmentBST();
+        PatientAppointment p1 = new PatientAppointment(1, "Test Patient", "09:00");
+        PatientAppointment p2 = new PatientAppointment(2, "Another Patient", "09:00");
+        bst.addAppointment(p1);
+        bst.addAppointment(p2); // Should not insert
+        assertNull(bst.getRoot().right);
+        assertNull(bst.getRoot().left);
+    }
+
+    @Test
+    public void testInOrderInsertionOrder() {
+        AppointmentBST bst = new AppointmentBST();
+        bst.addAppointment(new PatientAppointment(1, "A", "09:30"));
+        bst.addAppointment(new PatientAppointment(2, "B", "08:45"));
+        bst.addAppointment(new PatientAppointment(3, "C", "10:15"));
+
+        // Root should be 09:30, left 08:45, right 10:15
+        assertEquals("09:30", bst.getRoot().data.appointmentTime);
+        assertEquals("08:45", bst.getRoot().left.data.appointmentTime);
+        assertEquals("10:15", bst.getRoot().right.data.appointmentTime);
     }
 }
